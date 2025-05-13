404a3ac6deb62546b3da3622bfd4a0eb6b6dea80
Vinicius Stadler <vinicius.stadler@rrs-software.net>
2025-05-13 14:04:25 -0300

    add Transacional

---
+++ src/main/java/com/algaworks/id/RelatorioService.java
@@ -0,0 +1,26 @@
+package com.algaworks.id;
+
+import java.math.BigDecimal;
+
+import javax.inject.Inject;
+
+public class RelatorioService {
+	
+	@Inject
+	private Pedidos pedidos;
+	
+	public BigDecimal totalPedidosMesAtual() {
+		return pedidos.totalPedidosMesAtual();
+	}
+
+	public RelatorioService(Pedidos pedidos) {
+		this.pedidos = pedidos;
+	}
+	
+	public RelatorioService() {
+	}
+	
+	public void setPedidos(Pedidos pedidos) {
+		this.pedidos = pedidos;
+	}
+}
